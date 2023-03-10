f8bed3332995dc348f628a45b974ba8837b965a8
Miguel <miguelamaro100@gmail.com>
2023-03-09 22:07:16 -0500

    done

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 ```c
 8086 decoder
 file size: 2 bytes
+mov cx, bx
 10001001 11011001
 mov bit pattern: 100010
-d|w bits: 01
-mod bits: 11
+d|w bits: 01 (reg not dest | 16bit mov)
+mod bits: 11 (reg to reg op)
 reg bits: 011
 r/m bits: 001
 ```
--- src/decoder8086.c
+++ src/decoder8086.c
@@ -4,6 +4,7 @@
 
 #define Kilobytes(x) (x*1024)
 
+
 void PrintBinaryBytes(uint8_t *Buffer, uint32_t Size,
                       uint32_t ByteSplitMod, uint32_t ByteNewlineMod)
 {
@@ -58,21 +59,64 @@ uint32_t GetFileSize(FILE *File)
 int main()
 {
     printf("8086 decoder\n");
-    FILE *File = fopen(".\\decodeme", "rb");
+    FILE *InFile = fopen(".\\decodeme", "rb");
     
-    uint32_t Size = GetFileSize(File);
+    uint32_t Size = GetFileSize(InFile);
     printf("file size: %u bytes\n", Size);
     
     uint8_t *Buffer = malloc(Kilobytes(1));
-    fread(Buffer, Size, 1, File);
-    PrintBinaryBytes(Buffer, Size, 1, 2);
-    printf("mov bit pattern: "); PrintBinaryBits (Buffer, Size, 0, 6, 8, 16);
-    printf("d|w bits: "); PrintBinaryBits (Buffer, Size, 6, 2, 8, 16);
-    printf("mod bits: "); PrintBinaryBits (Buffer, Size, 8, 2, 8, 16);
-    printf("reg bits: "); PrintBinaryBits (Buffer, Size, 10, 3, 8, 16);
-    printf("r/m bits: "); PrintBinaryBits (Buffer, Size, 13, 3, 8, 16);
+    fread(Buffer, Size, 1, InFile);
+    fclose(InFile);
     
+    uint8_t *MemOp = Buffer;
+    PrintBinaryBytes(MemOp, Size, 1, 2);
+    printf("mov bit pattern: "); PrintBinaryBits (MemOp, Size, 0, 6, 8, 16);
+    printf("d|w bits: "); PrintBinaryBits (MemOp, Size, 6, 2, 8, 16);
+    printf("mod bits: "); PrintBinaryBits (MemOp, Size, 8, 2, 8, 16);
+    printf("reg bits: "); PrintBinaryBits (MemOp, Size, 10, 3, 8, 16);
+    printf("r/m bits: "); PrintBinaryBits (MemOp, Size, 13, 3, 8, 16);
+    
+    const char *RegTable    [] =  { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" };
+    const char *RegTableWide[] =  { "ax", "cx", "dx", "bx", "sb", "bp", "si", "di" };
+    uint8_t Instruction = (MemOp[0]>>2);
+    uint8_t Dest = ((MemOp[0]>>1)&0x1);
+    uint8_t Wide = ((MemOp[0]>>0)&0x1);
+    
+    uint8_t Mod  = ((MemOp[1]>>6)&0x3);
+    uint8_t Reg  = ((MemOp[1]>>3)&0x7);
+    uint8_t RM   = ((MemOp[1]>>0)&0x7);
+    
+    PrintBinaryBytes(&Instruction, 1, 1,1);
+    PrintBinaryBytes(&Dest, 1, 1,1);
+    PrintBinaryBytes(&Wide, 1, 1,1);
+    printf("\n");
+    PrintBinaryBytes(&Mod, 1, 1,1);
+    PrintBinaryBytes(&Reg, 1, 1,1);
+    PrintBinaryBytes(&RM , 1, 1,1);
+    
+    const char *Op       = NULL;
+    const char *OperandDest = NULL;
+    const char *OperandSrc  = NULL;
+    if(Mod == 0x3)
+    {
+        //Op type register to register
+        const char *RegNameReg = Wide==1?RegTableWide[Reg]:RegTable[Reg];
+        const char *RegNameRM  = Wide==1?RegTableWide[RM ]:RegTable[RM ];
+        OperandSrc  = Dest==1?RegNameRM :RegNameReg;
+        OperandDest = Dest==1?RegNameReg:RegNameRM;
+    }
+    if(Instruction == 0x22) Op = "mov";
+    
+    //Write to file
+    if(Op && OperandDest && OperandSrc)
+    {
+        FILE *OutFile = fopen("..\\data\\decoded.asm", "w+");
+        fputs("bit 16\n\n", OutFile);
+        fputs(Op      , OutFile); fputs(" " , OutFile);
+        fputs(OperandDest, OutFile); fputs(", ", OutFile);
+        fputs(OperandSrc , OutFile); fputs("\n", OutFile);
+        fclose(OutFile);
+    }
     printf("done...\n");
-    
     return 0;
 }
